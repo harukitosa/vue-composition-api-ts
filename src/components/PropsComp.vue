a91919e194211cf6b9c8ba49aaece7099fd81dcf
harukitosa <13haruki28@gmail.com>
2020-12-28 11:47:48 +0900

    fix composition api

---
+++ src/components/PropsComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="hello"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, SetupContext } from "vue";
+
+type Props = {
+    message: string;
+};
+export default defineComponent({
+    props: {
+        message: {
+            type: String,
+            default: "default value",
+        },
+    },
+    setup(props: Props, context: SetupContext) {
+        const upperCaseMessage = () => {
+            context.emit("emit-sample", props.message.toUpperCase());
+        };
+
+        return {
+            upperCaseMessage,
+        };
+    },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
