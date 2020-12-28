baa1da5ad2310b0639f26a61c56008fc5a14fe58
harukitosa <13haruki28@gmail.com>
2020-12-28 11:27:44 +0900

    Practice ref and reactive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class="hello"></div>
+    <div class="hello">
+        <h2>{{ state.messageOne }}</h2>
+        <h2>{{ messageTwo }}</h2>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 
 export default defineComponent({
     name: "HelloWorld",
+    setup() {
+        // reactiveとrefの使用方法
+        // 既存のものだとdataにあたるもの
+        const state = reactive<{ messageOne: string }>({
+            messageOne: "Hello",
+        });
+
+        const messageTwo = ref<string>("こんにちは");
+
+        return {
+            state,
+            messageTwo,
+        };
+    },
 });
 </script>
 
