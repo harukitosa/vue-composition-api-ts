deb28c5271cdabfcb40ddc4510209f4b4f70066c
harukitosa <13haruki28@gmail.com>
2020-12-28 11:19:44 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueCompositionApi from "@vue/composition-api";
 
+
+createApp(App).use(VueCompositionApi);
 createApp(App).mount('#app')
